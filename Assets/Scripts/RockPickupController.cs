9a43c2dacacd17e3816acb9e21177b11667ac1b1
Alexandra Magnusson <alexlm@pobox.com>
2023-06-07 15:34:41 -0400

    basic grab function, has bugs, grab by holding g

--- Assets/Scripts/RockPickupController.cs
+++ Assets/Scripts/RockPickupController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class RockPickupController : MonoBehaviour
 {
+    private bool isPickedUp = false;   // Flag to check if the object is picked up
+    private Transform playerHand;     // Reference to the player's hand transform
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void PickUp(Transform handTransform)
+    {
+        isPickedUp = true;
+        playerHand = handTransform;
+        transform.SetParent(playerHand);
+        transform.localPosition = Vector3.zero;
+        transform.rotation = Quaternion.identity;
+    }
+
+    public void Release()
+    {
+        isPickedUp = false;
+        transform.SetParent(null);
+    }
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             Debug.Log("destroy");
             Destroy(gameObject);
         }
-    }
-
-
+    }*/
 }
