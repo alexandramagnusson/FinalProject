748793c8446197cd406519b8e9db2c2ef15b445e
m-molina <m.k.molina@wustl.edu>
2023-06-07 06:51:28 -0500

    Igloo is now a door

--- Assets/Scripts/Igloo.cs
+++ Assets/Scripts/Igloo.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Igloo : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public string nextLevel = "";
+
+    private void Start()
     {
-        
+        if (nextLevel == "")
+        {
+            nextLevel = SceneManager.GetActiveScene().name;
+        }
     }
 
     // Update is called once per frame
+
     void Update()
     {
         
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        PlatformerController2D controller = other.gameObject.GetComponent<PlatformerController2D>();
+        if (controller != null)
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+
+    
+
 }
