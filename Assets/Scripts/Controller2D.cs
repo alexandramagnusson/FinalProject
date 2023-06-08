46eb2934ac863f7ad4f89d65d339bea1cdcec2a9
Alexandra Magnusson <alexlm@pobox.com>
2023-06-08 17:22:11 -0400

    script comments

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -4,74 +4,75 @@ using UnityEngine;
 
 public abstract class Controller2D : MonoBehaviour
 {
-    public float speed;
-    public bool grounded;
-    public LayerMask groundLayers;
-    public float groundRayLength = 0.1f;
-    public float groundRaySpread = 0.1f;
-    [HideInInspector] public Vector2 relativeVelocity = new Vector2();
+    public float speed; // The movement speed of the controller
+    public bool grounded; // Flag indicating if the controller is grounded
+    public LayerMask groundLayers; // The layers considered as ground
+    public float groundRayLength = 0.1f; // The length of the ground raycast
+    public float groundRaySpread = 0.1f; // The spread of the ground raycasts
+    [HideInInspector] public Vector2 relativeVelocity = new Vector2(); // The relative velocity of the controller
 
-    protected Rigidbody2D rb2d;
-    protected MovingPlatform onMovingPlatform;
+    protected Rigidbody2D rb2d; // Reference to the Rigidbody2D component
+    protected MovingPlatform onMovingPlatform; // Reference to the MovingPlatform the controller is standing on
 
     public virtual void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component
     }
 
     protected bool UpdateGrounding()
     {
-        Vector3 rayStart = transform.position + Vector3.up * groundRayLength;
-        Vector3 rayStartLeft = transform.position + Vector3.up * groundRayLength + Vector3.left * groundRaySpread;
-        Vector3 rayStartRight = transform.position + Vector3.up * groundRayLength + Vector3.right * groundRaySpread;
+        Vector3 rayStart = transform.position + Vector3.up * groundRayLength; // The starting position of the center ground raycast
+        Vector3 rayStartLeft = transform.position + Vector3.up * groundRayLength + Vector3.left * groundRaySpread; // The starting position of the left ground raycast
+        Vector3 rayStartRight = transform.position + Vector3.up * groundRayLength + Vector3.right * groundRaySpread; // The starting position of the right ground raycast
 
-        RaycastHit2D hit = Physics2D.Raycast(rayStart, Vector2.down, groundRayLength * 2, groundLayers);
-        RaycastHit2D hitLeft = Physics2D.Raycast(rayStartLeft, Vector2.down, groundRayLength * 2, groundLayers);
-        RaycastHit2D hitRight = Physics2D.Raycast(rayStartRight, Vector2.down, groundRayLength * 2, groundLayers);
+        RaycastHit2D hit = Physics2D.Raycast(rayStart, Vector2.down, groundRayLength * 2, groundLayers); // Perform a center ground raycast
+        RaycastHit2D hitLeft = Physics2D.Raycast(rayStartLeft, Vector2.down, groundRayLength * 2, groundLayers); // Perform a left ground raycast
+        RaycastHit2D hitRight = Physics2D.Raycast(rayStartRight, Vector2.down, groundRayLength * 2, groundLayers); // Perform a right ground raycast
 
-        Debug.DrawLine(rayStart, rayStart + Vector3.down * groundRayLength * 2, Color.red);
-        Debug.DrawLine(rayStartLeft, rayStartLeft + Vector3.down * groundRayLength * 2, Color.red);
-        Debug.DrawLine(rayStartRight, rayStartRight + Vector3.down * groundRayLength * 2, Color.red);
+        Debug.DrawLine(rayStart, rayStart + Vector3.down * groundRayLength * 2, Color.red); // Draw a red line for the center ground raycast
+        Debug.DrawLine(rayStartLeft, rayStartLeft + Vector3.down * groundRayLength * 2, Color.red); // Draw a red line for the left ground raycast
+        Debug.DrawLine(rayStartRight, rayStartRight + Vector3.down * groundRayLength * 2, Color.red); // Draw a red line for the right ground raycast
+
         if (hit.collider != null)
         {
-            grounded = true;
-            onMovingPlatform = hit.collider.gameObject.GetComponent<MovingPlatform>();
-            return true;
+            grounded = true; // Set grounded to true
+            onMovingPlatform = hit.collider.gameObject.GetComponent<MovingPlatform>(); // Get the MovingPlatform component if standing on one
+            return true; // Return true to indicate grounded status
         }
         else if (hitLeft.collider != null)
         {
-            grounded = true;
-            onMovingPlatform = hitLeft.collider.gameObject.GetComponent<MovingPlatform>();
-            return true;
+            grounded = true; // Set grounded to true
+            onMovingPlatform = hitLeft.collider.gameObject.GetComponent<MovingPlatform>(); // Get the MovingPlatform component if standing on one
+            return true; // Return true to indicate grounded status
         }
         else if (hitRight.collider != null)
         {
-            grounded = true;
-            onMovingPlatform = hitRight.collider.gameObject.GetComponent<MovingPlatform>();
-            return true;
+            grounded = true; // Set grounded to true
+            onMovingPlatform = hitRight.collider.gameObject.GetComponent<MovingPlatform>(); // Get the MovingPlatform component if standing on one
+            return true; // Return true to indicate grounded status
         }
         onMovingPlatform = null;
-        grounded = false;
-        return false;
+        grounded = false; // Set grounded to false if not standing on anything
+        return false; // Return false to indicate not grounded
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-     Controller2D controller = collision.gameObject.GetComponent<Controller2D>();
-     if(controller != null) {
-         Vector3 impactDirection = collision.gameObject.transform.position - transform.position;
-         if (collision.gameObject.name.Contains("Turbine"))
-         {
-             Heal(impactDirection);
-         }
-         else
-         {
-             Hurt(impactDirection);
-         }
-     }
+        Controller2D controller = collision.gameObject.GetComponent<Controller2D>();
+        if (controller != null)
+        {
+            Vector3 impactDirection = collision.gameObject.transform.position - transform.position;
+            if (collision.gameObject.name.Contains("Turbine"))
+            {
+                Heal(impactDirection); // Call the Heal method if colliding with an object containing "Turbine" in its name
+            }
+            else
+            {
+                Hurt(impactDirection); // Call the Hurt method for other collisions
+            }
+        }
     }
 
-    protected abstract void Hurt(Vector3 impactDirection);
-    public abstract void Heal(Vector3 impactDirection);
-
+    protected abstract void Hurt(Vector3 impactDirection); // Abstract method to handle when the controller gets hurt
+    public abstract void Heal(Vector3 impactDirection); // Abstract method to handle when the controller gets healed
 }
--- Assets/Scripts/PlatformerController2D.cs
+++ Assets/Scripts/PlatformerController2D.cs
@@ -5,103 +5,111 @@ using UnityEngine;
 
 public class PlatformerController2D : Controller2D
 {
-    public float jumpforce;
-    public int lives = 5;
-    private float inputX;
-    private SpriteRenderer sRenderer;
-    private bool invulnerable = false;
+    public float jumpforce; // The force applied when jumping
+    public int lives = 5; // The number of lives the player has
+    private float inputX; // The horizontal input value
+    private SpriteRenderer sRenderer; // Reference to the SpriteRenderer component
+    private bool invulnerable = false; // Flag indicating if the player is invulnerable
 
-    public AudioClip jumpsound;
-    public AudioClip hitsound;
-    public AudioClip coinsound;
-    public AudioClip killsound;
-    private AudioSource audioSource;
+    public AudioClip jumpsound; // Sound played when jumping
+    public AudioClip hitsound; // Sound played when the player gets hit
+    public AudioClip coinsound; // Sound played when collecting a coin
+    public AudioClip killsound; // Sound played when killing an enemy
+    private AudioSource audioSource; // Reference to the AudioSource component
 
     public override void Start()
     {
         base.Start();
-        sRenderer = GetComponent<SpriteRenderer>();
-        audioSource = GetComponent<AudioSource>();
-        HeartsUI.SetLives(lives);
+        sRenderer = GetComponent<SpriteRenderer>(); // Get the SpriteRenderer component
+        audioSource = GetComponent<AudioSource>(); // Get the AudioSource component
+        HeartsUI.SetLives(lives); // Set the initial number of lives in the HeartsUI
     }
 
     void Update()
     {
-        inputX = Input.GetAxis("Horizontal") * speed;
-        Vector2 vel = rb2d.velocity;
-        vel.x = inputX;
-        relativeVelocity = vel;
+        inputX = Input.GetAxis("Horizontal") * speed; // Get the horizontal input value and multiply it by the speed
+        Vector2 vel = rb2d.velocity; // Get the current velocity
+        vel.x = inputX; // Set the x component of the velocity
+        relativeVelocity = vel; // Update the relative velocity
 
-        UpdateGrounding();
-        if(onMovingPlatform != null)
+        UpdateGrounding(); // Update the grounding status
+        if (onMovingPlatform != null)
         {
-            vel.x += onMovingPlatform.rb2d.velocity.x;
+            vel.x += onMovingPlatform.rb2d.velocity.x; // Adjust the horizontal velocity when standing on a moving platform
         }
 
-        bool inputJump = Input.GetKeyDown(KeyCode.Space);
-        if (inputJump && grounded) {
-            AudioSource.PlayClipAtPoint(jumpsound, transform.position);
-            vel.y = jumpforce;
-            //Debug.Log("jump");
-            relativeVelocity.y = vel.y;
+        bool inputJump = Input.GetKeyDown(KeyCode.Space); // Check if the jump key is pressed
+        if (inputJump && grounded)
+        {
+            AudioSource.PlayClipAtPoint(jumpsound, transform.position); // Play the jump sound at the player's position
+            vel.y = jumpforce; // Set the vertical velocity to the jump force
+            relativeVelocity.y = vel.y; // Update the relative velocity
         }
-        rb2d.velocity = vel;
+        rb2d.velocity = vel; // Apply the updated velocity to the Rigidbody2D component
     }
 
     protected override void Hurt(Vector3 impactDirection)
     {
-        if (Mathf.Abs(impactDirection.x) > Mathf.Abs(impactDirection.y)) {
-            TakeDamage();
-        } else {
-            if(impactDirection.y > 0.0f) {
-                TakeDamage();
+        if (Mathf.Abs(impactDirection.x) > Mathf.Abs(impactDirection.y))
+        {
+            TakeDamage(); // Take damage if hit horizontally
+        }
+        else
+        {
+            if (impactDirection.y > 0.0f)
+            {
+                TakeDamage(); // Take damage if hit from below
             }
-            AudioSource.PlayClipAtPoint(killsound, transform.position);
+            AudioSource.PlayClipAtPoint(killsound, transform.position); // Play the kill sound at the player's position
             Vector2 vel = rb2d.velocity;
-            vel.y = jumpforce;
+            vel.y = jumpforce; // Set the vertical velocity to the jump force to simulate being knocked upwards
             rb2d.velocity = vel;
         }
     }
 
     public void TakeDamage()
     {
-        if (invulnerable) {
-            return;
+        if (invulnerable)
+        {
+            return; // Do nothing if the player is currently invulnerable
         }
-        AudioSource.PlayClipAtPoint(hitsound, transform.position);
-        lives--;
-        HeartsUI.RemoveHeart();
-        if(lives <= 0) {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        AudioSource.PlayClipAtPoint(hitsound, transform.position); // Play the hit sound at the player's position
+        lives--; // Decrease the number of lives
+        HeartsUI.RemoveHeart(); // Remove a heart from the HeartsUI
+        if (lives <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Restart the current scene if there are no more lives
         }
-        StartCoroutine(Invulnerability(1));
+        StartCoroutine(Invulnerability(1)); // Start the invulnerability coroutine for a specified time
     }
 
-        public override void Heal(Vector3 impactDirection)
+    public override void Heal(Vector3 impactDirection)
+    {
+        if (invulnerable)
         {
-            if (invulnerable) {
-                return;
-            }
-
-            lives++;
-            HeartsUI.AddHeart();
+            return; // Do nothing if the player is currently invulnerable
         }
+
+        lives++; // Increase the number of lives
+        HeartsUI.AddHeart(); // Add a heart to the HeartsUI
+    }
 
     public void CollectCoin()
     {
-        AudioSource.PlayClipAtPoint(coinsound, transform.position);
+        AudioSource.PlayClipAtPoint(coinsound, transform.position); // Play the coin sound at the player's position
     }
 
     IEnumerator Invulnerability(float time)
     {
         Debug.Log("Invulnerable " + Time.time);
-        invulnerable = true;
-        for(int i = 0; i < time/0.2f; i++) {
-            sRenderer.color = Color.red;
-            yield return new WaitForSeconds(0.1f);
-            sRenderer.color = Color.white;
-            yield return new WaitForSeconds(0.1f);
+        invulnerable = true; // Set the invulnerability flag to true
+        for (int i = 0; i < time / 0.2f; i++)
+        {
+            sRenderer.color = Color.red; // Change the sprite color to red
+            yield return new WaitForSeconds(0.1f); // Wait for a short period
+            sRenderer.color = Color.white; // Change the sprite color back to white
+            yield return new WaitForSeconds(0.1f); // Wait for a short period
         }
-        invulnerable = false;
+        invulnerable = false; // Set the invulnerability flag to false
     }
 }
